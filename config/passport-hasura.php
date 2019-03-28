6a745a564e0435f76ac7fb16d06bca7fc5427ca5
Jason McCallister <themccallister@gmail.com>
2019-03-27 20:35:01 -0400

    Add configs and inline docs

--- config/passport-hasura.php
+++ config/passport-hasura.php
@@ -2,6 +2,22 @@
 
 return [
     /*
+     * Should the package intercept Passport signing and modify the token for Hasura?
+     * This is useful if you decided to keep Passport confgiured and working as is,
+     * and you want to use this packages /hasura/auth endpoint instead.
+     */
+    'enabled' => env('PASSPORT_HASURA_ENABLED', true),
+
+    /*
+     * The default role that will be set for Hasura JWTs. This should be set to a role
+     * that has very strict permissions. If you have a role will basic read access,
+     * such as anonymous like the Hasura documentation recommends under roles
+     * (https://docs.hasura.io/1.0/graphql/manual/auth/roles-variables.html#roles)
+     * then that role should be set here.
+     */
+    'default_role' => env('PASSPORT_HASURA_DEFAULT_ROLE', 'user'),
+
+    /*
      * Set the claims namespace Hasura is configured to receive. Default is https://hasura.io/claims.
      * There is no need to modify this unless you modified the claims namespace in Hasura.
      */
